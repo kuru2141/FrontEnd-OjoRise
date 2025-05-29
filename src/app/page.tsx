8e60ef503e20ea95150eff24733e297564bd5e7b
yunhyuk-choi <cyh9355@yonsei.ac.kr>
2025-05-29 09:45:52 +0900

    OJORISE-13 remove build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import HamburgerIcon from "@/components/common/HambugIcon";
 import { LoginForm } from "@/components/login-form";
 import Image from "next/image";
 
@@ -6,7 +5,6 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-
         <Image
           className="dark:invert"
           src="/next.svg"
