32898ecd7caaaed041d522e9157b126b0c7ab821
dogeonkim1 <caddibo4@naver.com>
2025-05-30 16:34:57 +0900

    OJORISE-23 FEAT: 모바일 환경과 웹 환경에서의 레이아웃을 다르게 설정

--- src/components/common/AppHeader.tsx
+++ src/components/common/AppHeader.tsx
@@ -8,6 +8,14 @@ type Props = {
     toggleMenu: () => void;
 }
 
+const menuItems = [
+    {label: "MENU1", href:"/"},
+    {label: "MENU2", href:"/"},
+    {label: "MENU3", href:"/"},
+    {label: "MENU4", href:"/"},
+    {label: "MENU5", href:"/"},
+]
+
 
 function AppHeader({isOpen, toggleMenu}: Props) {
     const [isScrolled, setIsScrolled] = useState(false);
@@ -22,8 +30,27 @@ function AppHeader({isOpen, toggleMenu}: Props) {
 
     return (
         <header className={`pl-[24px] pr-[16px] xl:pl-[48px] xl:pr-[40px] fixed bg-white w-full z-49 ${isScrolled? "shadow-md":""}`}>
-            <div className="max-w-[1660px] inner flex align-middle items-center flex-wrap mt-0 mb-0 mr-auto ml-auto h-[56px] xl:h-[80px] justify-between">
-                <HamburgerIcon isOpen={isOpen} toggleMenu={toggleMenu} />
+            <div className="max-w-[1480px] inner flex align-middle items-center flex-wrap mt-0 mb-0 mr-auto ml-auto h-[56px] xl:h-[80px] justify-between">
+                {/*logo*/}
+                <div className="text-lg font-bold">OjoRise</div>
+
+                {/*desktopMenu(xl)*/}
+                <nav className="hidden xl:flex space-x-6">
+                    {menuItems.map((item, index) => (
+                        <a
+                        key={item.label}
+                        href={item.href}
+                        className="text-base font-medium text-neutral-800 hover:text-gray-600 transition"
+                        >
+                            {item.label}
+                        </a>
+                    ))}
+                </nav>
+
+                {/*mobile(xl under)*/}
+                <div className="xl:hidden">
+                    <HamburgerIcon isOpen={isOpen} toggleMenu={toggleMenu} />
+                </div>
             </div>
         </header>
     );
