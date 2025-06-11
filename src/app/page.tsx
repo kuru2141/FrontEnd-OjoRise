c5e4d37ef95176e09d5b2961cbf628ef5fb14fb0
qowldk <qowldk1030@gmail.com>
2025-06-11 21:50:42 +0900

    OJORISE-56 FEAT: 홈페이지에 컴포넌트 합치기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,24 @@ import { LoginForm } from "@/components/login-form";
 import Image from "next/image";
 import PlanBox from "@/components/page-component/main-page/plan/PlanBox";
 import BannerCarousel from "@/components/page-component/BannerCarousel";
+import CompareModeToggle from "@/components/page-component/main-page/recommanded-list/CompareModeToggle";
+import RecommendedPlanList from "@/components/page-component/main-page/recommanded-list/RecommendedPlanList";
+import LikedPlansList from "@/components/page-component/main-page/recommanded-list/LikedPlansList";
+import SelectedPlanViewer from "@/components/page-component/main-page/recommanded-list/SelectedPlanViewer";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <PlanBox />
-        <BannerCarousel/>
+        <BannerCarousel />
+        <section className="w-full max-w-[768px] mt-10">
+          <h2 className="text-3xl font-bold mb-4 mt-1">요금제 비교하기</h2>
+          <CompareModeToggle />
+          <RecommendedPlanList />
+          <LikedPlansList />
+          <SelectedPlanViewer />
+        </section>
         <Image
           className="dark:invert"
           src="/next.svg"
