2785b7e8f4858f4b39f8db641cf908252da12bf5
sujinRo <shtnwls1111@ewhain.net>
2025-06-14 16:51:16 +0900

    OJORISE-63 FEAT: 스크린샷 이미지 버튼

--- src/components/page-component/signup/ScreenshotOCR.tsx
+++ src/components/page-component/signup/ScreenshotOCR.tsx
@@ -1,10 +1,14 @@
 'use client';
 
 import { planOCR } from '@/app/api/signup';
+import { Button } from '@/components/ui/button';
 import { OCR_PROMPT } from '@/prompt/OCRPrompt';
 import { ChatResult, ResultItem } from '@/types/OCR';
 import { useMutation } from '@tanstack/react-query';
-import { ChangeEvent, memo, useCallback, useState } from 'react';
+import clsx from 'clsx';
+import { result } from 'lodash';
+import Image from 'next/image';
+import { ChangeEvent, memo, useCallback, useEffect, useRef, useState } from 'react';
 
 interface ScreenshotOCRProps {
   onComplete: (result: ResultItem) => void;
@@ -12,6 +16,7 @@ interface ScreenshotOCRProps {
 
 function ScreenshotOCR({onComplete}: ScreenshotOCRProps) {
   const [imgFile, setImgFile] = useState<File | null>(null);
+  const fileInputRef = useRef<HTMLInputElement>(null);
   
   const planOCRMutation = useMutation({
     mutationFn: planOCR,
@@ -49,22 +54,27 @@ function ScreenshotOCR({onComplete}: ScreenshotOCRProps) {
     const file = e.target.files?.[0];
     if (!file) return;
     setImgFile(file);
-  },[]);
-
+  }, []);
+  
   const handleClick = () => {
+    fileInputRef.current?.click();
+  }
+
+  useEffect(() => {
     if (imgFile) {
       const formData = new FormData();
       formData.append('image', imgFile);
       
       planOCRMutation.mutate(formData);
     }
-  }
+  }, [imgFile]);
 
   return (
-    <div className="p-4">
-      <input type='file' onChange={handleChange} />
-      <div className='w-[100px] h-[100px]' onClick={handleClick}>버튼</div>
-    </div>
+    <Button variant='outline' className={clsx(imgFile ? 'border-[var(--color-primary-medium)]' : 'border-[var(--color-gray-40)]', 'flex flex-row gap-[10px] content-center justify-center bg-white border-[1px] border-solid rounded-[5px] h-[60px] w-[337px] cursor-pointer')} onClick={handleClick}>
+      <input className='hidden' type='file' onChange={handleChange} ref={fileInputRef}/>
+      <Image src={`${imgFile?'/afterOCR.svg':'/beforeOCR.svg'}`} alt='capture' width={30} height={30} />
+      <p className={clsx(imgFile ? 'text-[var(--color-primary-medium)]' : 'text-[var(--color-gray-40)]', 'font-bold text-lg leading-[30px]')}>캡처 이미지로 회원가입 채우기</p>
+    </Button>
   );
 }
 
