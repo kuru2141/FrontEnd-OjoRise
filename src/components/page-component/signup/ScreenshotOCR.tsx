011cc493dc79cf2e78781244978b42767beba171
sujinRo <shtnwls1111@ewhain.net>
2025-06-15 15:52:28 +0900

    OJORISE-63 STYLE: imgFile이 있을 때, style 안 적용되는 부분 수정

--- src/components/page-component/signup/ScreenshotOCR.tsx
+++ src/components/page-component/signup/ScreenshotOCR.tsx
@@ -5,7 +5,7 @@ import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
 import { OCR_PROMPT } from '@/prompt/OCRPrompt';
 import { ResultItem } from '@/types/ocr';
-import { extractJsonFromGpt } from '@/utils/extractJsonFromGPT';
+import { extractJsonFromGpt } from '@/utils/extractJsonFromGpt';
 import { useMutation } from '@tanstack/react-query';
 import Image from 'next/image';
 import { ChangeEvent, memo, useCallback, useEffect, useRef, useState } from 'react';
@@ -57,10 +57,10 @@ function ScreenshotOCR({onComplete}: ScreenshotOCRProps) {
   }, [imgFile]);
 
   return (
-    <Button variant='outline' className={cn(imgFile && 'border-[var(--color-primary-medium)]', 'border-[var(--color-gray-40)] flex flex-row gap-[10px] content-center justify-center bg-white border-[1px] border-solid rounded-[5px] h-[60px] w-[337px] cursor-pointer')} onClick={handleClick}>
+    <Button variant='outline' className={cn('border-[var(--color-gray-40)] flex flex-row gap-[10px] content-center justify-center bg-white border-[1px] border-solid rounded-[5px] h-[60px] w-[337px] cursor-pointer', imgFile && 'border-[var(--color-primary-medium)]')} onClick={handleClick}>
       <input className='hidden' type='file' onChange={handleChange} ref={fileInputRef}/>
       <Image src={`${imgFile?'/afterOCR.svg':'/beforeOCR.svg'}`} alt='capture' width={30} height={30} />
-      <p className={cn(imgFile && 'text-[var(--color-primary-medium)]', 'text-[var(--color-gray-40)] font-bold text-lg leading-[30px]')}>캡처 이미지로 회원가입 채우기</p>
+      <p className={cn('text-[var(--color-gray-40)] font-bold text-lg leading-[30px]', imgFile && 'text-[var(--color-primary-medium)]')}>캡처 이미지로 회원가입 채우기</p>
     </Button>
   );
 }
