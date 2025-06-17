bd98db0e531ae128dde0a5f8cf9b6fd4abff6697
yunhyuk-choi <cyh9355@yonsei.ac.kr>
2025-06-17 10:24:45 +0900

    OJORISE-56 changed home route

--- src/components/page-component/main-page/MainPage.tsx
+++ src/components/page-component/main-page/MainPage.tsx
@@ -1,12 +1,18 @@
+import BannerCarousel from "../BannerCarousel";
 import ScrollChart from "./compare-chart/ScrollChart";
 import CompareModeToggle from "./compare-plans/CompareModeToggle";
-import LikedPlansList from "./liked-list/LikedPlansList";
+import LikedPlansList from "./compare-plans/LikedPlansList";
 import RecommendedPlanList from "./compare-plans/RecommendedPlanList";
 import SelectedPlanViewer from "./compare-plans/SelectedPlanViewer";
+import PlanBox from "./plan/PlanBox";
 
 export default function MainPage() {
   return (
     <main className="flex flex-col items-center bg-white px-4 pb-20">
+      <section className="w-full max-w-[768px] mt-30 flex flex-col items-center gap-20">
+        <PlanBox />
+        <BannerCarousel />
+      </section>
       <section className="w-full max-w-[768px] mt-10">
         <h2 className="text-3xl font-bold mb-4 mt-10">요금제 비교하기</h2>
         <CompareModeToggle />
