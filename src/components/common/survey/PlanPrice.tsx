8bc7a2c62c85773b4e88c0197982890225dba0c5
sujinRo <shtnwls1111@ewhain.net>
2025-06-18 16:58:13 +0900

    OJORISE-63 FEAT: planPrice 입력 input type text로 변경

--- src/components/common/survey/PlanPrice.tsx
+++ src/components/common/survey/PlanPrice.tsx
@@ -14,13 +14,11 @@ export function PlanPrice() {
       <p className="font-bold text-[18px] mb-3 mt-4">요금제 금액 선택</p>
       <Input
         className="relative w-[260px] h-[50px] focus:outline-none"
-        type="number"
+        type="text"
         inputMode="numeric"
         placeholder="요금제 가격을 입력해 주세요"
         value={data.planPrice}
         onChange={handleChange}
-        min={0}
-        step={1}
       />
     </div>
   );
