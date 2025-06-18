cd79c99de9957ba6a4c34cc5b14180cfa399be41
sujinRo <shtnwls1111@ewhain.net>
2025-06-18 22:51:05 +0900

    OJORISE-63 FEAT: OCR isPending 처리

--- src/components/page-component/signup/ScreenshotOCR.tsx
+++ src/components/page-component/signup/ScreenshotOCR.tsx
@@ -8,6 +8,7 @@ import { useOCRToGptMutation } from '@/hooks/useOCRToGptMutation';
 import Image from 'next/image';
 import { ChangeEvent, memo, useEffect, useRef, useState } from 'react';
 import PopoverBox from './PopoverBox';
+import LoadingProgressCircle from '@/components/common/progress/LoadingProgressCircle';
 
 interface ScreenshotOCRProps {
   onComplete: (result: ResultItem) => void;
@@ -16,7 +17,7 @@ interface ScreenshotOCRProps {
 function ScreenshotOCR({onComplete}: ScreenshotOCRProps) {
   const [imgFile, setImgFile] = useState<File | undefined>();
   const fileInputRef = useRef<HTMLInputElement>(null);
-  const { mutate } = useOCRToGptMutation(onComplete);
+  const { mutate, isPending } = useOCRToGptMutation(onComplete);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
@@ -45,7 +46,7 @@ function ScreenshotOCR({onComplete}: ScreenshotOCRProps) {
     <div className='mb-7 w-[260px]'>
       <Button variant='outline' className={cn('border-gray-40 flex flex-row gap-[10px] content-center justify-center bg-white border-[1px] border-solid rounded-[5px] h-[50px] w-full cursor-pointer mb-[5px]', imgFile && 'border-primary-medium')} onClick={handleClick}>
         <input className='hidden' type='file' onChange={handleChange} ref={fileInputRef} />
-        <Image src={`${imgFile ? '/afterOCR.svg' : '/beforeOCR.svg'}`} alt='capture' width={20} height={20} />
+        {isPending ? <LoadingProgressCircle/> : <Image src={`${imgFile ? '/afterOCR.svg' : '/beforeOCR.svg'}`} alt='capture' width={20} height={20} />}
         <p className={cn('text-gray-40 font-bold text-base leading-[30px]', imgFile && 'text-primary-medium')}>캡처 이미지로 회원가입 채우기</p>
       </Button>
       <PopoverBox/>
