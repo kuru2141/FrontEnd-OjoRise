cfd57d0493310a9668a3085d61dfeafbe322db4d
leedaye0412 <neoldy00@naver.com>
2025-06-19 13:35:21 +0900

    OJORISE-77 FEAT: 회원정보 수정 완료 모달 생성

--- src/components/page-component/edit-survey/EditSurveyPage.tsx
+++ src/components/page-component/edit-survey/EditSurveyPage.tsx
@@ -1,15 +1,37 @@
+"use client";
+
+import { useState } from "react";
+import UpdateModal from "./UpdateModal";
+
 const EditSurveyPage = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
+  const handleWithdraw = () => {
+    //수정api 보내기 hook에서 성공하면 마이페이지로 이동하는거 추가하기
+    closeModal();
+  };
+
   return (
     <div className="flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
-          <div className="w-full max-w-sm flex flex-col items-start text-left gap-12 overflow-hidden">
-            <div>
-              <p className="font-bold text-[32px] mb-3">회원 정보 수정하기</p>
-              <p className="text-[18px] text-gray-60">
-                정보를 작성해주시면 적합한 요금제를 추천해드려요
-              </p>
-            </div>
-          </div>
+      <div className="w-full max-w-sm flex flex-col items-start text-left gap-12 overflow-hidden">
+        <div>
+          <p className="font-bold text-[32px] mb-3">회원 정보 수정하기</p>
+          <p className="text-[18px] text-gray-60">
+            정보를 작성해주시면 적합한 요금제를 추천해드려요
+          </p>
         </div>
+      </div>
+      <button onClick={openModal} className="text-gray-40 mt-10 text-[8px] sm:text-[12px]">
+        수정하기
+      </button>
+      <UpdateModal
+        isOpen={isModalOpen}
+        onCancel={() => setIsModalOpen(false)}
+        onConfirm={handleWithdraw}
+      />
+    </div>
   );
 };
 
