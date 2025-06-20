9eb32a21555cb59030f70d0d43efd54d087fd615
sujinRo <shtnwls1111@ewhain.net>
2025-06-20 22:08:53 +0900

    OJORISE-87 FEAT: RadarChart 기준 그래프에서 비교 그래프로 이동되도록 animation 구현

--- src/components/page-component/main-page/compare-chart/RadarChart.tsx
+++ src/components/page-component/main-page/compare-chart/RadarChart.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { memo, useEffect, useRef, useState } from "react";
+import { memo, useEffect, useRef, useState, useMemo } from "react";
 import { ChartOptions, ChartData } from "chart.js/auto";
 import {
   Chart as ChartJS,
@@ -13,7 +13,6 @@ import {
   Chart,
 } from 'chart.js';
 import { Radar } from "react-chartjs-2";
-
 
 function RadarChart() {
   ChartJS.register(
@@ -31,24 +30,45 @@ function RadarChart() {
   },[]);
   
   const labels = ['월정액', '혜택', '문자', '음성통화', '데이터'];
+  const baseData = [28, 48, 40, 19, 96];
+  const targetData = [65, 59, 90, 81, 56];
+  const [enableAnimation, setEnableAnimation] = useState(false);
 
-  const data: ChartData<'radar'> = {
+  const [data, setData] =  useState<ChartData<'radar'>>({
     labels,
-    datasets: [   
-      {
-        label: '비교 요금제',
-        data: [28, 48, 40, 19, 96],
-        backgroundColor: 'rgba(3, 199, 90, 0.4)', 
-        borderColor: 'rgba(3, 199, 90, 1)',
-      },
+    datasets: [  
       {
         label: '기준 요금제',
-        data: [65, 59, 90, 81, 56],
+        data: baseData,
+        backgroundColor: 'rgba(3, 199, 90, 0.4)', 
+        borderColor: 'rgba(3, 199, 90, 1)',  
+      },
+      {
+        label: '비교 요금제',
+        data: baseData,
         backgroundColor: 'rgba(252, 255, 99, 0.4)', 
-        borderColor: 'rgba(252, 255, 99, 1)',   
+        borderColor: 'rgba(252, 255, 99, 1)', 
       }
     ]
-  };
+  });
+
+  const changeToCompare = () => {
+    setEnableAnimation(true);
+    const timeout = setTimeout(() => {
+      setData(prev => ({
+        ...prev,
+        datasets:[
+          {
+            ...prev.datasets[0],
+            data: targetData
+          }, prev.datasets[1]
+        ]
+      }))
+      setTimeout(() => setEnableAnimation(false), 1500);
+    }, 600);
+
+    return () => clearTimeout(timeout);
+  }
 
   const legendPositionPlugin = {
     id: 'legendPositionPlugin',
@@ -66,10 +86,16 @@ function RadarChart() {
     }
   };
 
-  const options: ChartOptions<'radar'> = {
+  const options = useMemo<ChartOptions<'radar'>>(() => ({
     responsive: true, // 부모 넓이 따라감
     maintainAspectRatio: false, // 부모의 가로세로 비율 따라감
     devicePixelRatio: dpr, // 화질 개선
+    animation: enableAnimation
+      ? {
+          duration: 1500,
+          easing: 'easeOutQuart',
+        }
+      : false,
     plugins: {
       title: {
         display: false
@@ -87,7 +113,7 @@ function RadarChart() {
           boxHeight: 10,
           color: 'var(--color-gray-100)',
           generateLabels: (chart) => {
-            return [...chart.data.datasets].reverse().map((dataset, i) => ({
+            return [...chart.data.datasets].map((dataset, i) => ({
               text: dataset.label ?? '',
               fillStyle: i === 0 ? '#FCFF63' : '#03C75A',
               strokeStyle: 'transparent',
@@ -126,11 +152,8 @@ function RadarChart() {
         beginAtZero: true,
         max: 100
       }
-    },
-    animation: {
-      duration: 250
-    },
-  }
+    }
+  }), [dpr, enableAnimation]);
 
   const chartRef = useRef<HTMLDivElement>(null);
   const [chartKey, setChartKey] = useState(0);
@@ -140,7 +163,19 @@ function RadarChart() {
     const observer = new IntersectionObserver(
       ([entry]) => {
         if (entry.isIntersecting) {
-          setChartKey((prev) => prev + 1);
+          setEnableAnimation(false);
+          setData(prev => ({
+            ...prev,
+            datasets: [
+              {
+                ...prev.datasets[0],
+                data: baseData,
+              },
+              prev.datasets[1]
+            ]
+          }));
+          setChartKey(prev => prev+1);
+          changeToCompare();
         }
       },
       {
