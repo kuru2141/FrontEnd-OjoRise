d72ebf9d4084dce546fbea6916780e5f89d8eb84
leedaye0412 <neoldy00@naver.com>
2025-06-24 02:36:12 +0900

    OJORISE-103 FIX: 요금제 정보에 따른 UI 수정

--- src/components/page-component/explore-plans/PlanDipCard.tsx
+++ src/components/page-component/explore-plans/PlanDipCard.tsx
@@ -22,6 +22,21 @@ export const PlanDipCard = ({ plan, isLiked, onToggle, openModal }: PlanDipCardP
     onToggle(plan.planId); // 설문 했으면 찜 등록
   };
 
+  const renderDataInfo = () => {
+    if (plan.baseDataGb === "무제한") {
+      return "데이터 무제한";
+    } else if (plan.baseDataGb === '0') {
+      return `데이터 매일 ${plan.dailyDataGb}GB`;
+    } else {
+      return `데이터 ${plan.baseDataGb}GB`;
+    }
+  };
+
+  const renderSharingInfo = () => {
+    if (plan.sharingDataGb === '0') return "";
+    return ` + 테더링/쉐어링 ${plan.sharingDataGb}GB`;
+  };
+
   return (
     <div className="flex flex-col w-full p-4 rounded-2xl shadow-soft bg-white mt-5 gap-2">
       <div className="flex flex-row justify-between items-start">
@@ -41,7 +56,8 @@ export const PlanDipCard = ({ plan, isLiked, onToggle, openModal }: PlanDipCardP
 
       <div>
         <h3 className="text-lg font-bold text-black">
-          데이터 {plan.baseDataGb} + 테더링/쉐어링 {plan.sharingDataGb}GB
+          {renderDataInfo()}
+          {renderSharingInfo()}
         </h3>
         <p className="text-sm text-gray-500">
           통화 {plan.voiceCallPrice} | 문자 {plan.sms}
