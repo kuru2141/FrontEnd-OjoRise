c216a0d9b2b92825c7b779d71fe5a7b42b37b290
leedaye0412 <neoldy00@naver.com>
2025-06-24 13:25:05 +0900

    OJORISE-107 FEAT: 요금제 둘러보기, 마이페이지, 회원정보수정, 회원가입 페이지 서브타이틀 추가

--- src/app/mypage/edit-survey/page.tsx
+++ src/app/mypage/edit-survey/page.tsx
@@ -1,5 +1,9 @@
 import EditSurveyPage from "@/components/page-component/edit-survey/EditSurveyPage";
 
+export const metadata = {
+  title: "회원정보 수정",
+};
+
 export default function EditSurveyPageRoute() {
   return <EditSurveyPage />;
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,5 +1,9 @@
 import SignupPage from "@/components/page-component/signup/SignupPage";
 
+export const metadata = {
+  title: "회원가입",
+};
+
 export default function SignupPageRoute() {
   return <SignupPage />;
 }
