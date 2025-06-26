0ddd2bccc7945d538a7fc86cfb9e6fc47047f5fa
qowldk <qowldk1030@gmail.com>
2025-06-26 14:43:46 +0900

    OJORISE-163 FIX: meta content imageUrl 로 수정

--- src/components/page-component/main-page/compare-plans/SelectedPlanViewer.tsx
+++ src/components/page-component/main-page/compare-plans/SelectedPlanViewer.tsx
@@ -19,7 +19,7 @@ export default function SelectedPlanViewer() {
 
   if (visibleLength === 0)
     return (
-      <section className="mt-10 mb-10">
+      <section className="text-xl md:text-2xl mt-10 mb-10">
         <h2 className="text-xl md:text-2xl font-bold mb-4">선택한 요금제</h2>
         <div className="text-center">
           <p className="text-gray-500 mb-4mt-4 text-sm md:text-lg">
@@ -31,12 +31,12 @@ export default function SelectedPlanViewer() {
 
   return (
     <section className="mt-10 mb-10">
-      <h2 className="text-2xl font-bold mb-4">선택한 요금제</h2>
+      <h2 className="text-xl md:text-2xl font-bold mb-4">선택한 요금제</h2>
       {selectedPlans.length === 1 ? (
         <div className="flex justify-start w-full">
           <div className="min-w-[330px] px-8 py-4 rounded-xl bg-white w-fit border border-gray-200 shadow-[0_4px_12px_rgba(0,0,0,0.08)]">
             <div className="text-gray-40 text-sm">기준 요금제</div>
-            <div className="text-xl font-semibold">{selectedPlans[0]?.name}</div>
+            <div className="text-lg font-semibold">{selectedPlans[0]?.name}</div>
           </div>
         </div>
       ) : selectedPlans.length === 2 ? (
