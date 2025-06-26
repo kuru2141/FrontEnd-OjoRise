c29e16140a334c34efa96351b24696c1b488c536
sujinRo <shtnwls1111@ewhain.net>
2025-06-26 10:12:14 +0900

    OJORISE-154 FEAT: 요금제 연령 테스트 UI 수정

--- src/components/page-component/test-age-page/AgeTest.tsx
+++ src/components/page-component/test-age-page/AgeTest.tsx
@@ -96,34 +96,41 @@ function AgeTest() {
   ];
 
   return (
-    <div className="h-screen flex flex-col justify-center items-center relative overflow-hidden  px-4 py-2 rounded-xl shadow-lg">
-      {lines.map(({ prompt, selectList, handler, value, placeHolder }, i) => {
-        const state = i < current ? "previous" : i === current ? "active" : "next";
-
-        return (
-          <MotionSelectLine key={i} state={state}>
-            <div className="flex flex-row text-black  text-5xl">
-              {prompt[0]}
-              <ItemSelector
-                handler={handler}
-                isSelect={i !== 1}
-                selectList={selectList}
-                value={value}
-                placehoder={placeHolder}
-              />
-              {prompt[1]}
-            </div>
-          </MotionSelectLine>
-        );
-      })}
-      <Button
-        className={`absolute bottom-10 ${
-          current === 3 || selectedPlan !== "" ? "" : "hidden"
-        } text-white`}
-        onClick={handleClickSubmit}
-      >
-        확인하러 가기
-      </Button>
+    <div className="flex flex-col justify-center items-center">
+      <div className="w-screen px-[20px] md:w-[531px] md:px-0 mt-[100px] text-gray-100 font-bold text-[20px] md:text-[32px]">
+        <span>나의 <span className="text-primary-medium">통신 연령</span>은?</span>
+        <div className="w-full flex flex-wrap font-medium mt-[35px] text-[14px] md:text-[18px]">내 요금제 정보와 나이를 입력하면<br/>
+        내 요금제를 통한 통신 연령과 실제 나이를 통한 추천 요금제를 알려드립니다.</div>
+        <div className="h-[480px] md:h-[602px] flex flex-col justify-center relative">
+          {lines.map(({ prompt, selectList, handler, value, placeHolder }, i) => {
+            const state = i < current ? "previous" : i === current ? "active" : "next";
+            
+            return (
+            <MotionSelectLine key={i} state={state}>
+              <div className="flex flex-row gap-[10px] md:gap-[15px] items-center">
+                {prompt[0]}
+                <ItemSelector
+                  handler={handler}
+                  isSelect={i !== 1}
+                  selectList={selectList}
+                  value={value}
+                  placehoder={placeHolder}
+                />
+                {prompt[1]}
+              </div>
+            </MotionSelectLine>
+          );
+        })}
+        <Button
+          className={`absolute bottom-10 w-full h-[55px] md:h-[70px] text-[18px] md:text-[24px] ${
+           current === 3 && selectedPlan !== "" ? "" : "hidden"
+          } text-white`}
+          onClick={handleClickSubmit}
+        >
+          분석받기
+        </Button>
+      </div>
+      </div>
     </div>
   );
 }
--- src/components/page-component/test-age-page/ItemSelector.tsx
+++ src/components/page-component/test-age-page/ItemSelector.tsx
@@ -22,8 +22,8 @@ function ItemSelector({ selectList, value, handler, isSelect, placehoder }: Item
   if (isSelect)
     return (
       <Select onValueChange={handler} value={value}>
-        <SelectTrigger className="text-5xl text-primary-medium border-primary-medium">
-          <SelectValue placeholder={placehoder} className="h-[250px]" />
+        <SelectTrigger className="text-[18px] border-gray-20 w-[180px] md:w-[270px] !h-[55px] dm:!h-[38px]">
+          <SelectValue placeholder={placehoder} />
         </SelectTrigger>
         <SelectContent>
           {selectList.map((item, i) => (
@@ -46,14 +46,34 @@ function ItemSelector({ selectList, value, handler, isSelect, placehoder }: Item
   return (
     <Autocomplete
       id={`test_select`}
-      sx={{ width: 300 }}
+      sx={{ 
+        width: {xs:180, md:270}, 
+        height: {xs:55, md:38},
+        '& .MuiInputBase-input': {
+          fontSize: 18,
+          fontWeight: 'bold',
+        },
+        '& .MuiOutlinedInput-root': {
+          borderRadius: '8px',        
+          backgroundColor: '#fff',                 
+          '& fieldset': {
+            borderColor: '#EAEAEA',  
+            boxShadow: '0 1px 2px 0 rgba(0, 0, 0, 0.05)', 
+          },
+          '&:hover fieldset': {
+            borderColor: '#EAEAEA',    
+          },
+          '&.Mui-focused fieldset': {
+            borderColor: '#EAEAEA',   
+          }
+      }}}
       options={selectList}
       autoHighlight
       getOptionLabel={(option) => {
         if (typeof option === "string") return option;
         return (option as ApiPlan).name;
       }}
-      renderInput={(params) => <TextField {...params} variant="outlined" label={placehoder} />}
+      renderInput={(params) => <TextField {...params} variant="outlined" label="" />}
       onChange={handleChange}
       onInputChange={handleChange}
       value={value}
