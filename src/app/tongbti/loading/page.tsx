5e219ae5f83ca037a02863a89437e5b37bac1b17
yunhyuk-choi <cyh9355@yonsei.ac.kr>
2025-06-26 10:49:56 +0900

    OJORISE-145 remove loading page

--- src/app/tongbti/loading/page.tsx
+++
@@ -1,9 +0,0 @@
-import TongBTILoadingPage from "@/components/page-component/tongbti/LoadingPage";
-
-export const metadata = {
-  title: "YoPle - 통신 연령 테스트 결과 도출 중...",
-};
-
-export default function LoadingPageRoute() {
-  return <TongBTILoadingPage />;
-}
